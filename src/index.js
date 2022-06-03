6e917892efca541d5b06d693ccec226060c276f0
Faycal <99132896+Faycal88@users.noreply.github.com>
2022-06-03 12:55:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
