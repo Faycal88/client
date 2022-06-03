7f4510bdb6963c2db872e89504a5e7a26f0cc88d
Faycal <99132896+Faycal88@users.noreply.github.com>
2022-06-03 12:43:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById('root');
 root.render(
   <Provider store={store}>
     <App />
